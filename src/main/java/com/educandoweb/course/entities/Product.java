ea3f508607e7b53ab81fc01f5ec65920a57a7128
Fernando Barbosa Santos <muribinhalive@gmail.com>
2023-11-12 01:45:00 -0300

    Category

---
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -0,0 +1,14 @@
+package com.educandoweb.course.entities;
+
+public class Product {
+	
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+	
+	public Product() {
+	}
+
+}
