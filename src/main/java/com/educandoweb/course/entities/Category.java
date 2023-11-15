3104f5a6069f7431c5644f9de5a67cc7fc49fe80
Fernando Barbosa Santos <muribinhalive@gmail.com>
2023-11-15 16:46:45 -0300

    Product

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -1,13 +1,16 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "tb_category")
@@ -19,6 +22,9 @@ public class Category implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Transient  //impede que o JPA interprete esse comando abaixo
+	private Set<Product> products = new HashSet<>();
 	
 	
 	public Category() {
@@ -50,6 +56,9 @@ public class Category implements Serializable{
 		this.name = name;
 	}
 
+	public void setProducts(Set<Product> products) {
+		this.products = products;
+	}
 
 	@Override
 	public int hashCode() {
@@ -68,6 +77,8 @@ public class Category implements Serializable{
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
+
+	
 	
 	
 	
